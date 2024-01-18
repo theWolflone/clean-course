f46babceda35681f2f79fdbd00a78988fe5f8cf0
Santiago Lobo <lobo09.slu@gmail.com>
2024-01-17 19:12:40 -0600

    02-name-types Ejercicios de nombres correctos de variables dependiendo del tipo de dato

---
+++ src/clean-code/02-name-types.ts
@@ -0,0 +1,50 @@
+(() => {
+
+      const celsiusTemperatures = [33.6, 12.34];
+  
+      const serverIpAddress = '123.123.123.123';
+  
+      const users = [{id: 1, email: 'fernando@google.com'},{ id: 2, email: 'juan@google.com' }, { id: 3, email: 'melissa@google.com' }];
+  
+      const usersEmails = users.map( user => user.email );
+  
+      const canJump = false;
+      const isRunning = true;
+      const hasItems = true;
+      const isLoading = false;
+  
+      const startTime = new Date().getTime();
+      //....
+      // 3 doritos después
+      //...
+      const endTime = new Date().getTime() - startTime;
+  
+  
+      // Funciones
+      function getBooksList() {
+          throw new Error('Function not implemented.');
+      }
+  
+      function getBooksUrl( url: string) {
+          throw new Error('Function not implemented.');
+      }
+      
+      function getSquareArea( square: number ) {
+          throw new Error('Function not implemented.');
+      }
+  
+      function printJob() {
+          throw new Error('Function not implemented.');
+      }
+      
+      
+  
+  
+  
+  
+  })();
+  
+  
+  
+  
+  
